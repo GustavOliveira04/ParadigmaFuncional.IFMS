041fdf78dd9fd73f066a6e7a9a85e6a36e70a7b5
GustavOliveira04 <gufreitas254@gmail.com>
2025-01-28 20:30:17 -0400

    Aula 11

--- aula-10/frontend.js
+++ aula-10/frontend.js
@@ -1,8 +1,12 @@
 document.getElementById("btnSalvar").addEventListener("click", async () => {
 
-    try {
-        const response = await axios.post('http://localhost:3000/api/dados', {nome, idade, proffisao});
+    const nome = document.getElementById('nome').value;
+    const idade = Number(document.getElementById('idade').value);
+    const profissao = document.getElementById('profissao').value;
 
+    try {
+        const response = await axios.post('http://localhost:3000/api/dados', {nome, idade, profissao});
+        console.log(response);
         document.getElementById('mensagem').innerHTML =
         `<div class="alert alert-success">Dados salvos com sucesso!</div>`;
     }catch (error) {
--- aula-9/backend.js
+++ aula-9/backend.js
@@ -1,78 +1,67 @@
-//npm install mysql2
-const mysql = require('mysql2')
+const mysql = require('mysql2');
 const express = require('express');
 const cors = require('cors');
 
-const porta = 3030;
-const app = express();
 
+const porta = 3000;
+const app = express();
 app.use(cors());
 
 const connection = mysql.createConnection({
-    host: 'localhost', 
+    host: 'localhost',
     user: 'root',
     password: 'ifms',
     database: 'tads'
 });
 
-app.use(express.json())
-
-//POST SALVAR NO BANCO
+app.use(express.json());
+//post salvar no banco
 app.post('/api/dados', (req, res) => {
-
-    const dados = req.body
-
-    try{
-        if(!dados.nome || typeof dados.nome !=='string' || dados.nome.trim()==='') {
-            throw new Error('Nome inválido. O nome deve ser uma string não vazia.');
+    const dados = req.body;
+    try
+    {
+        if(!dados.nome||typeof dados.nome !== 'string' || dados.nome.trim() ===''){
+            throw new Error('Nome invalido');
         }
-        if(!dados.idade || typeof dados.idade !== 'number' || !Number.isInteger(dados.idade) || dados.idade <= 0) {
-            throw new Error('Idade inválida. A idade deve ser um número inteiro maior que 0');
+        if(!dados.idade || typeof dados.idade !== 'number'|| !Number.isInteger(dados.idade) || dados.idade < 0){
+            throw new Error('Idade invalida');
         }
-        if(!dados.profissao || typeof dados.profissao !== 'string' || dados.profissao.trim()==='') {
-            throw new Error('Profissão inválida. A profissão deve ser uma string não vazia');            
+        if(!dados.profissao||typeof dados.profissao!=='string' || dados.profissao.trim() ===''){
+            throw new Error('Profissao invalida');
         }
-        // Inserção no banco de dados
 
-    
-    //if
     connection.query(
-        'INSERT INTO pessoa (nome, idade, profissao) VALUES (?, ?, ?)',
-        [dados.nome, dados.idade, dados.profissao],
-        (err, results) =>{
-            if (err) {
-                console.error('Erro ao inserir no banco de dados: ' + err.stack);
-                res.status(500).json({message: 'Erro ao inserir dados no banco de dados.'});
+        'INSERT INTO pessoa (nome, idade, profissao) VALUES (?,?,?)',
+        [dados.nome, dados.idade,dados.profissao],
+        (error, results) => {
+            if(error) {
+                console.error('Error ao inserir no banco' + error.stack);
+                res.status(500).json({message: 'Error'});
                 return;
             }
             res.json({
-                message: 'Dados recebidos e inseridos com sucesso!',
-                dados: dados,
-                resultadoBanco: results
+                message: 'Inserido com sucesso',
+                dados:dados,
+                resultadoBanco:results
             });
         }
     );
 
-} catch (error){
-    //Retorno em caso de erro na validação
-    res.status(400).json({message:error.message});
-}
-
+    }catch(error) {
+        //retorno em caso de erro na validação
+        res.status(400).json({error: error.message});
+    }
 });
-
-//Endpoint para buscar dados do banco de dados
-app.get('/api/getdados', (req, res) => {
-    //Realiza uma consulta ao banco de dados para buscar dados
-    connection.query('SELECT * FROM pessoa', (err, results) => {
-        if (err) {
-            console.error('Erro ao consultar dados: ' + err.stack);
-    res.status(500).json({message: 'Erro ao consultar dados no banco de dados.'});
+//endpoint para buscar dados do banco de dados
+app.get('/api/getdados',(req, res) => {
+    //realiza uma consulta ao banco
+    connection.query('select * from pessoa', (error, results) => {
+        if(error){
+            console.error('erro ao consultar dados: '+ error.stack);
+            res.status(500).json({message: 'Error'});
             return;
         }
-        res.json({
-            message: 'Dados recuperados com sucesso!',
-            dados: results
-        });
-    });
-});
-app.listen(porta, () => { console.log(`Servidor em execução:http://localhost:${porta}`);});
+        res.json({message: 'Inserido com sucesso',
+            dados:results
+        }); });});
+app.listen(porta,() => {console.log(`servidor em execução:http://localhost:${porta}`);});
